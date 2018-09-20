cca46f02c3e640a67edd4c99786a4bc932b3834c
heremneha <nehak829.7@gmail.com>
2018-09-20 21:19:40 +0530

    neha

---
+++ src/Controller/RecipesController.php
@@ -0,0 +1,80 @@
+<?php
+namespace App\Controller;
+
+use App\Controller\AppController;
+class RecipesController extends AppController
+{
+    public function initialize()
+    {
+        parent::initialize();
+        $this->loadComponent('RequestHandler');
+    }
+
+    public function index()
+    {
+        $recipes = $this->Recipes->find('all');
+        $this->set([
+            'recipes' => $recipes,
+            '_serialize' => ['recipes']
+        ]);
+    }
+
+    public function view($id)
+    {
+        $recipe = $this->Recipes->get($id);
+        $this->set([
+            'recipe' => $recipe,
+            '_serialize' => ['recipe']
+        ]);
+    }
+
+    public function add()
+    {
+        $recipe = $this->Recipes->newEntity($this->request->getData());
+        if ($this->Recipes->save($recipe)) {
+            $message = 'Saved';
+        } else {
+            $message = 'Error';
+        }
+        $this->set([
+            'message' => $message,
+            'recipe' => $recipe,
+            '_serialize' => ['message', 'recipe']
+        ]);
+    }
+
+    public function edit($id)
+    {
+        $recipe = $this->Recipes->get($id);
+        if ($this->request->is(['post', 'put'])) {
+            $recipe = $this->Recipes->patchEntity($recipe, $this->request->getData());
+            if ($this->Recipes->save($recipe)) {
+                $message = 'Saved';
+            } else {
+                $message = 'Error';
+            }
+        }
+        $this->set([
+            'message' => $message,
+            '_serialize' => ['message']
+        ]);
+    }
+
+    public function delete($id)
+    {
+        $recipe = $this->Recipes->get($id);
+        $message = 'Deleted';
+        if (!$this->Recipes->delete($recipe)) {
+            $message = 'Error';
+        }
+        $this->set([
+            'message' => $message,
+            '_serialize' => ['message']
+        ]);
+    }
+    public function beforeFilter(Event $event) {
+        if (in_array($this->Recipes->add())) {
+            $this->eventManager()->off($this->Csrf);
+        }
+    }
+}
